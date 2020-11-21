16b71e0aa1deeccb002d726b173c9643621ceb34
fznsakib <fznsakib@gmail.com>
2020-11-21 15:54:55 +0400

    Create campaign page file

---
+++ front/src/pages/CampaignUsers.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex bg-image flex-center">
+          <h1>Manage Users</h1>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+  export default {
+    events: {
+
+    },
+    data() {
+      return {
+        // Store dummy data here
+        // People
+        // Roles
+      }
+    },
+  }
+</script>
+
+<style>
+</style>
