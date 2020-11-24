9b76a89436c081dd8ce36b09aa35de2c158fd03b
ntad2 <ntad2@hotmail.com>
2020-11-23 20:04:29 -0800

    #43 Created component <messages-header>

---
+++ front/src/pages/Messages.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="q-pa-sm">
+    <messages-header></messages-header>
+  </q-page>
+</template>
+
+<script>
+  import MessagesHeader from '../components/messages/Header';
+
+  export default {
+    name: "Messages",
+    components: {
+      MessagesHeader
+    }
+  }
+</script>
