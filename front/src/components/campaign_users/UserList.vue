89a102d0ceeff67c82d119d0a1fc001389592651
fznsakib <fznsakib@gmail.com>
2020-12-11 16:40:45 +0000

    Created campaign user list

---
+++ front/src/components/campaign_users/UserList.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="row q-col-gutter-sm q-py-sm q-px-sm">
+        <div class="col-6 text-h5">
+            Campaign Admin Team
+        </div>
+        <div class="col-6">
+            <q-input v-model="search" label="Search Users" dense>
+                <template v-slot:append>
+                    <q-icon name="search" />
+                </template>
+            </q-input>
+        </div>
+        <div class="col-12">
+            <q-table
+                title=""
+                :data="data"
+                :columns="columns"
+                row-key="name"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+
+import CampaignUsersUserItem from 'components/campaign_users/UserItem.vue'
+
+export default {
+    name: "CampaignUsersUserList",
+    components: {
+        CampaignUsersUserItem
+    },
+    data() {
+        return {
+            search: '',
+            columns: [
+                {
+                    name: 'name',
+                    required: true,
+                    label: 'Name',
+                    align: 'left',
+                    field: row => row.name,
+                    format: val => `${val}`,
+                    sortable: true
+                },
+                { name: 'role', align: 'left', label: 'Role', field: 'role', sortable: true },
+                { name: 'status', align: 'left', label: 'Status', field: 'status', sortable: true },
+            ],
+            data: [
+                {
+                    name: 'John Doe',
+                    role: 'Campaign Admin',
+                    status: 'Active'
+                },
+                {
+                    name: 'Jane Doe',
+                    role: 'Campaign Manager',
+                    status: 'Active'
+                },
+                {
+                    name: 'Anna Mull',
+                    role: 'Volunteer',
+                    status: 'Active'
+                },
+            ]
+        }
+    }
+}
+</script> 
--- front/src/pages/CampaignUsers.vue
+++ front/src/pages/CampaignUsers.vue
@@ -3,19 +3,21 @@
     <q-page-container>
       <q-page class="q-pa-xl window-height window-width">
         <div class="row justify-center">
-          <div class="col-4">
+          <div class="col-5">
             <campaign-users-header/>
           </div>
-          <div class="col-4">
+          <div class="col-5">
             <campaign-users-invite-button/>
           </div>
         </div>
         <div class="row justify-center">
-          <div class="col-4">
+          <div class="col-3">
             <campaign-users-group-list/>
           </div>
+          <div class="col-7">
+            <campaign-users-user-list/>
+          </div>
         </div>
-        
       </q-page>
     </q-page-container>
   </q-layout>
@@ -26,13 +28,15 @@
   import CampaignUsersHeader from 'components/campaign_users/Header.vue'
   import CampaignUsersInviteButton from 'components/campaign_users/InviteButton.vue'
   import CampaignUsersGroupList from 'components/campaign_users/GroupList.vue'
+  import CampaignUsersUserList from 'components/campaign_users/UserList.vue'
 
   export default {
     name: "CampaignUsers",
     components: {
       CampaignUsersHeader,
       CampaignUsersInviteButton,
-      CampaignUsersGroupList
+      CampaignUsersGroupList,
+      CampaignUsersUserList
     },
     data() {
       return {
