c200520dd533be82614c50d50bc80845dcd7da54
Md Ashiqur Rahman <md.ashik.ar@gmail.com>
2020-12-18 16:27:24 +0600

    cmps: added wall and wall-header

---
+++ front/src/components/wall/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="wallHeader">
+    <q-tabs
+      v-model="tab"
+      dense
+      align="left"
+      :breakpoint="0"
+    >
+      <q-tab name="wall"> <span class="font-medium text-base capitalize" >Wall</span> </q-tab>
+      <q-tab name="campaign"><span class="font-medium text-base text-grey-6 capitalize" >Campaign</span></q-tab>
+    </q-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostHeader",
+  data() {
+    return {
+      tab: "wall",
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/src/pages/Wall.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="q-pa-md bg-white">
+    <div class="row q-col-gutter-md">
+      <div class="col-md-8 offset-md-2">
+        <post-header />
+        <q-separator />
+      </div>
+      
+    </div>
+  </q-page>
+</template>
+
+<script>
+import PostHeader from '../components/wall/Header';
+    export default {
+        name: "Wall",
+        components: {
+          PostHeader
+        },
+        data() {
+            return {
+                posts: []
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.bg {
+  background-color: #ffffff;
+}
+</style>
