18cc8731d5d2df782660bd0e5252f40aca84f95c
Njsteelejr <njsteelejr@gmail.com>
2021-01-21 20:28:25 -0500

    Filled in Terms page

--- front/src/pages/Terms.vue
+++ front/src/pages/Terms.vue
@@ -5,7 +5,44 @@
       <q-card class="col-6 q-pa-md">
         <q-card-section>
           <div class="text-h6">Our terms</div>
-          Text goes here
+          <br/><br/>
+          Except as expressly otherwise noted, all data you download from HOPE are made available to you in accordance with the Creative Commons Attribution-4.0 International License (https://creativecommons.org/licenses/by/4.0/) , as amended and updated by Creative Commons from time to time (“CC BY-4.0”), which is incorporated herein by this reference. The CC BY-4.0 License allows you to freely share and adapt HOPE content for any purpose, even commercial ones, provided that you give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use. Appropriate Credit means, if supplied, you must provide the name of the creator and attribution parties, a copyright notice, a license notice, and a disclaimer notice. Notice: No warranties are given. The license may not give you all of the permissions necessary for your intended use. For example, other rights such as publicity, privacy or moral rights may limit how you use the material. Please consult the CC BY-4.0.
+          <br/><br/>
+          Updated: June 2020, Version 2.0
+          <br/><br/>
+          <div class="text-h6">Our privacy policy</div>
+          <br/><br/>
+          <div class="text-primary">Introduction</div>
+          <br/>
+          Thank you for using HOPE. Please read this Privacy Policy carefully. This Privacy Policy is a part of our Terms. If you have any questions or comments, or if you want to update, delete, or change any personal information you have submitted on the Service, please e-mail the Center for Public Health Law Research at Temple University Beasley School of Law (CPHLR) at monqcle.info@temple.edu. HOPE is owned and operated by CPHLR at 1819 N. Broad Street, Barrack Hall, Suite 300, Philadelphia, Pennsylvania, 19122. The Center for Public Health Law Research will be referred to as “CPHLR” under these Terms. HOPE will be referred to as the “Service.” Users of this Service will be referred to “User” or “you” under these Terms. In the course of providing the Services, CPHLR may collect Personal Information about a User. This Privacy Policy is effective with respect to any data CPHLR has collected or will collect, about and/or from you, according to our Terms of Use. If there are any changes to this Privacy Policy, CPHLR will post them on the Service. Any changes will be effective as of the last date we post on the Service. CPHLR will not treat information of any open account differently from any other open account. If we ever change our Privacy Policy to expand our rights to use or disclose personal information for marketing purpose, you will be able to opt out without termination.
+          <br/><br/>
+          <div class="text-primary">Infromation Collected</div>
+          <br/>
+          When you create an account to use the Service or send us an e-mail, you are giving us information that CPHLR collects. That information may include your IP address, name, physical address, email address, phone number, credit card information, and other details like occupation and demographic information. By giving CPHLR this information, you consent to your information being collected, used, disclosed, and stored by CPHLR, only as described in our Privacy Policy. CPHLR may get information about how and when you use the Service. This information may include your IP address, time, date, browser used, and actions taken by you within the Service. When you create an account to use the Service, CPHLR stores cookies, or strings of code, on your computer. CPHLR uses those cookies to collect information about when you visit our Service, your browser type and version, your operating system, and other similar information. You may disable cookies, but doing so may make the Service more difficult to use.
+          <br/><br/>
+          <div class="text-primary">Use and Disclosure of Personal Information</div>
+          <br/>
+          CPHLR may use and disclose your personal information only as follows: 1. To send you Service alert e-mails – we may e-mail you to inform you of any changes to our Services, including version updates, planned outages, or changes to our Terms or Privacy Policy. 2. To enforce compliance with our Terms and applicable law to help prevent violations. 3. To provide customer support. 4. To protect our rights and safety as well as the rights and safety of other Users and third parties. 5. To meet legal requirements and provide information to representatives and advisors necessary to help CPHLR comply with any legal, financial, or security requirements. 6. To communicate with you about your account. 7. To transfer your information in the event of a sale, merger, or acquisition. Any acquirer will be subject to our obligations under this Privacy Policy. You will be notified of the change by posting a notice on our Service. 8. To inquire about the use of the Service.
+          <br/><br/>
+          <div class="text-primary">Public Information and Third Parties</div>
+          <br/>
+          Our Service includes links to other websites, whose privacy policies might be different than that of CPHLR. If you submit any personal information to any of those websites, your information will be governed by their privacy policies.
+          <br/><br/>
+          <div class="text-primary">Notice of Breach of Security</div>
+          <br/>
+          If a security breach causes an unauthorized use of our system that materially affects you, CPHLR will notify you as soon as possible and report our actions in response.
+          <br/><br/>
+          <div class="text-primary">Safeguarding Information</div>
+          <br/>
+          Accounts for the Service requires a username and password to log in. You are responsible for keeping your username and password secure. Do not disclose your username and password to a third party.
+          <br/><br/>
+          <div class="text-primary">Accuracy of Data, Transparency, and Choice</div>
+          <br/>
+          If your account data changes, you are responsible for notifying CPHLR of those changes. This includes switching to a new e-mail address. We only store data for as long as is reasonably required to fulfill the purposes that gave CPHLR the right to access it in the first place. We will provide you with access to any personal information about you that we hold within 30 days of any request for that information you make by contacting us at monqcle.info@temple.edu. Unless prohibited by law, we will remove any personal information about you from our servers at your request.
+          <br/><br/>
+          <div class="text-primary">Enforcement</div>
+          <br/>
+          We regularly review our compliance with this Privacy Policy.
         </q-card-section>
       </q-card>
       <div class="col-3" />
