231ee0ca4c7ded56d57ff1e3664e7a0b2997607b
c-49 <reithc4@gmail.com>
2021-02-22 19:01:43 -0600

    Nothing layout

---
+++ front/src/layouts/Nothing.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- Route -->
+  <q-layout view="hHh LpR fFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'LayoutNothing'
+}
+</script>
