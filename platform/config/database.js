5f75d136e2a0186ce02fe9b2869dc88c55ad2160
Ajay Kumar <31384241+ajaysheoran2323@users.noreply.github.com>
2021-02-26 11:13:03 +0530

    Update database.js

--- platform/config/database.js
+++ platform/config/database.js
@@ -4,12 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', 'temp/data.db'),
+        client: 'mysql',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'platform'),
+        username: env('DATABASE_USERNAME', 'platform'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {}
     },
   },
 });
