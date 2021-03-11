e4348d2ac6e69a85739ae00a2740570330016f20
Md Ashiqur Rahman <md.ashik.ar@gmail.com>
2021-03-12 05:08:52 +0600

    files name changed to hyphen-case, reslove vue warning - non-primitive value as key

---
+++ front/src/components/campaign/campaign-comment-item.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="">
+    <campaign-comment-list
+      v-for="comment in comments"
+      :key="comment.id"
+      :name="comment.name"
+      :comment="comment.comment"
+      :username="comment.username"
+      :time="comment.time"
+    ></campaign-comment-list>
+
+    <!-- <div v-for="n in 4" :key="n">
+
+      <div class="row q-pa-sm">
+        <div class="col-2 col-md-1">
+          <q-avatar rounded>
+            <img src="https://cdn.quasar.dev/img/avatar.png">
+          </q-avatar>
+        </div>
+        <div class="col-10 col-md-11 q-px-md">
+          <p>Winston <br> <span class="text-grey">@winston</span></p>
+          <p class="text-grey">9:30am</p>
+          <p class="text-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil
+            praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores</p>
+
+          <q-btn outline padding="none" size="1.1rem" flat no-caps label="Reply"/>
+        </div>
+      </div>
+
+      <hr style="color: #fcfcfc; width: 100%"/>
+
+    </div> -->
+  </div>
+</template>
+
+<script>
+import CampaignCommentList from "components/campaign/campaign-comment-list.vue";
+export default {
+  name: "campaign_comment_item",
+  components: {
+    CampaignCommentList,
+  },
+  data() {
+    return {
+      comments: [
+        {
+          id: 1,
+          name: "Winston",
+          username: "winston",
+          comment:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihilpraesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores",
+          time: "9.30am",
+        },
+        {
+          id: 2,
+          name: "Winston",
+          username: "winston",
+          comment:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihilpraesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores",
+          time: "9.30am",
+        },
+        {
+          id: 3,
+          name: "Winston",
+          username: "winston",
+          comment:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihilpraesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores",
+          time: "9.30am",
+        },
+        {
+          id: 4,
+          name: "Winston",
+          username: "winston",
+          comment:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihilpraesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores",
+          time: "9.30am",
+        },
+      ],
+    };
+  },
+};
+</script>
